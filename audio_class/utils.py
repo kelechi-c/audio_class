efe9e5a7896bd111ecb723d9ed6c13981ef44f42
Tensor <narutokelechi@gmail.com>
2024-07-26 05:05:40 +0100

    add: functions for audio loading and spectrogram display

--- audio_class/utils.py
+++ audio_class/utils.py
@@ -3,6 +3,7 @@ import os
 import torchaudio
 from pydub import AudioSegment
 from transformers import WhisperFeatureExtractor
+from matplotlib import pyplot as plt
 
 
 class config:
@@ -11,6 +12,11 @@ class config:
     max_duration = 30
     sample_rate = 16000
     epochs = 50
+    num_classes = 18
+
+
+feature_extractor = WhisperFeatureExtractor.from_pretrained(
+    "openai/whisper-small")
 
 
 def load_audio(file: str):
@@ -19,10 +25,27 @@ def load_audio(file: str):
         file = f'{file.split('.')[0]}.wav'
         audio.export(file)
 
-    waveform, sr = torchaudio.load(file)
+    waveform, sr = librosa.load(file, sr=config.sample_rate)
 
     return waveform
 
 
-def extract_audio_features(audio):
-    pass
+def prepare_dataset(sample):
+    audio_sample = sample["audio"]
+    audio_feature = feature_extractor(
+        audio_sample["array"], sampling_rate=audio_sample["sampling_rate"], padding=True
+    )
+
+    return audio_feature
+
+
+def display_melspec(audio):
+    array, sr = audio["array"], audio["sampling_rate"]
+
+    specgram = librosa.feature.melspectrogram(
+        y=array, sr=sr, n_mels=128, fmax=8000)
+
+    plt.figure().set_figwidth(12)
+    librosa.display.specshow(specgram, x_axis="time",
+                             y_axis="mel", sr=sr, fmax=8000)
+    plt.colorbar()
