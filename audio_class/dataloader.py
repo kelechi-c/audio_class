d4c121e139dcd65fe0aab2d7977012746ce06c6d
Tensor <narutokelechi@gmail.com>
2024-07-27 17:12:16 +0100

    add: dataset class, dataloaders and train splits

--- audio_class/dataloader.py
+++ audio_class/dataloader.py
@@ -1,28 +1,43 @@
 import torch
-import librosa
-import torchaudio
-import numpy as np
+import math
 from datasets import load_dataset
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
+from utils import config, get_spectrogram
+from tqdm.auto import tqdm
 
-dataset_id = "lewtun/music_genres"
+hfdata = load_dataset(config.dataset_id, split="train", streaming=True)
 
-hfdata = load_dataset(dataset_id, split="train")  # , streaming=True)
+hfdata = hfdata.take(config.split)
+hfdata_list = list(hfdata)
 
-audio_files = hfdata["audio"]
-genre_ids = hfdata["genre_id"]
-genres = hfdata["genre"]
+audio_arrays = [k for k in tqdm(hfdata_list["audio"])]
+labels = [v for v in tqdm(hfdata_list["genre_ids"])]
 
 
 class MusicData(Dataset):
-    def __init__(self, files, labels, extractor):
+    def __init__(self, audio_arrays=audio_arrays, labels=labels):
         super().__init__()
-        self.audio_files = files
+        self.audio_arrays = audio_arrays
         self.labels = labels
-        self.extractor = extractor
 
     def __len__(self):
-        return len(self.audio_files)
+        return len(self.audio_arrays)
 
     def __getitem__(self, idx):
-        pass
+        audio = get_spectrogram(self.audio_arrays[idx])
+        audio_tensor = torch.tensor(audio)
+
+        label = torch.tensor(self.labels[idx])
+
+        return audio_tensor, label
+
+
+music_genre_data = MusicData()
+
+train_size = math.floor(len(music_genre_data) * config.train_split)
+val_size = len(music_genre_data) - train_size
+
+train_data, valid_data = random_split(music_genre_data, (train_size, val_size))
+
+train_loader = DataLoader(train_data, config.batch_size, shuffle=True)
+valid_loader = DataLoader(valid_data, config.batch_size, shuffle=False)
