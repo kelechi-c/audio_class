6c9edfae8c0606675ec07393ef00991e327fe2a5
Tensor <narutokelechi@gmail.com>
2024-07-29 04:55:24 +0100

    modify: rewrite convnet and forward pass, add torch.compile

--- audio_class/audio_classifier.py
+++ audio_class/audio_classifier.py
@@ -3,37 +3,37 @@ from torch import nn
 from utils import config
 
 
+# model class => convnet for audio classification
 class MusiClass(nn.Module):
-    def __init__(self, out_classes=10):
+    def __init__(self, out_classes=18):
         super().__init__()
-        self.convnet = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=2),
+        self.audio_convnet = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=2),
             nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.MaxPool2d(2, 2),
             nn.ReLU(),
         )
 
         self.linear_fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(16384, 128),
-            nn.Dropout2d(0.1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.Linear(128, out_classes),
+            nn.Linear(128 * 4 * 4, 128), nn.ReLU(), nn.Linear(128, out_classes)
         )
 
-    def forward(self, input: torch.Tensor):
-        input = input.unsqueeze(1)
-        x = self.convnet(input)
+    def forward(self, x: torch.Tensor):
+        x = x.unsqueeze(1)
+
+        x = self.audio_convnet(x)
+
+        x = x.view(-1, 128 * 4 * 4)  # flatten output
         x = self.linear_fc(x)
 
         return x
 
 
 classifier = MusiClass().to(config.device)
+classifier = torch.compile(classifier)
