0da686e72bc15ce0e0d4bb0e4ac0f09995a34054
Tensor <narutokelechi@gmail.com>
2024-07-29 12:35:38 +0100

    [modify] fix model output dimension and dtype, sample prediction

--- audio_class/audio_classifier.py
+++ audio_class/audio_classifier.py
@@ -1,9 +1,11 @@
 import torch
 from torch import nn
 from utils import config
-
+from dataloader import train_loader
 
 # model class => convnet for audio classification
+
+
 class MusiClass(nn.Module):
     def __init__(self, out_classes=18):
         super().__init__()
@@ -20,20 +22,45 @@ class MusiClass(nn.Module):
             nn.ReLU(),
         )
 
+        # Calculate the output size after convolutions and pooling
+        self.conv_output_size = self._get_conv_output_size((128, 2000))
+
         self.linear_fc = nn.Sequential(
-            nn.Linear(128 * 4 * 4, 128), nn.ReLU(), nn.Linear(128, out_classes)
+            nn.Linear(self.conv_output_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, out_classes),
         )
 
-    def forward(self, x: torch.Tensor):
-        x = x.unsqueeze(1)
+    def _get_conv_output_size(self, shape):
+        batch_size = 1
+        input = torch.autograd.Variable(torch.rand(batch_size, 1, *shape))
+        output_feat = self.audio_convnet(input)
+        n_size = output_feat.data.view(batch_size, -1).size(1)
+        return n_size
 
+    def forward(self, x: torch.Tensor):
+        # x shape: (batch_size, 128, 2000)
+        x = x.unsqueeze(1)  # Now shape is (batch_size, 1, 128, 2000)
         x = self.audio_convnet(x)
 
-        x = x.view(-1, 128 * 4 * 4)  # flatten output
+        x = x.view(x.size(0), -1)  # Flatten
+
         x = self.linear_fc(x)
 
-        return x
+        return x  # Shape: (batch_size, out_classes)
 
 
-classifier = MusiClass().to(config.device)
+classifier = MusiClass()
+
+classifier = classifier.to(torch.bfloat16).to(config.device)
 classifier = torch.compile(classifier)
+
+x = next(iter(train_loader))[0]
+print(x.shape)
+
+k = classifier(x)
+
+torch.cuda.empty_cache()
+
+print(f"model pred shape => {k.shape}")
+print(k)
